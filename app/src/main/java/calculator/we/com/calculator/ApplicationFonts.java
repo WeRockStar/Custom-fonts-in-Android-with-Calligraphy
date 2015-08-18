1c853f0245a9911dffc82828b8a962fb3e933799
comfreestyle <vr_freestyle@outlook.com>
2015-08-19 00:28:59 +0700

    Implement Calligraphy

--- app/src/main/java/calculator/we/com/calculator/ApplicationFonts.java
+++ app/src/main/java/calculator/we/com/calculator/ApplicationFonts.java
@@ -1,7 +1,21 @@
 package calculator.we.com.calculator;
 
+import android.app.Application;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by Kotchaphan Muangsan on 19/8/2558.
  */
-public class ApplicationFonts {
+public class ApplicationFonts extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Roboto-Bold.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
+    }
 }
--- app/src/main/java/calculator/we/com/calculator/MainActivity.java
+++ app/src/main/java/calculator/we/com/calculator/MainActivity.java
@@ -1,9 +1,12 @@
 package calculator.we.com.calculator;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +16,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 }
